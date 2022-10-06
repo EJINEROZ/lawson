e53986e82460525708fa3c09e7e37842ceeb0c83
ejineroz <ejineroz@pop-os.localdomain>
2022-10-06 23:30:04 +0100

    second

---
+++ index.js
@@ -0,0 +1,43 @@
+require("./index.css");
+
+function $parcel$interopDefault(a) {
+  return a && a.__esModule ? a.default : a;
+}
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+var $d43ba8995c46d720$exports = {};
+
+$parcel$export($d43ba8995c46d720$exports, "card", () => $d43ba8995c46d720$export$aa3e815946b80764, (v) => $d43ba8995c46d720$export$aa3e815946b80764 = v);
+$parcel$export($d43ba8995c46d720$exports, "link", () => $d43ba8995c46d720$export$9c30223ca0a664fb, (v) => $d43ba8995c46d720$export$9c30223ca0a664fb = v);
+$parcel$export($d43ba8995c46d720$exports, "img", () => $d43ba8995c46d720$export$463b44d9bf3628be, (v) => $d43ba8995c46d720$export$463b44d9bf3628be = v);
+$parcel$export($d43ba8995c46d720$exports, "container", () => $d43ba8995c46d720$export$34e0f9847d4c02dd, (v) => $d43ba8995c46d720$export$34e0f9847d4c02dd = v);
+$parcel$export($d43ba8995c46d720$exports, "heading", () => $d43ba8995c46d720$export$46e4a324ac90507f, (v) => $d43ba8995c46d720$export$46e4a324ac90507f = v);
+var $d43ba8995c46d720$export$aa3e815946b80764;
+var $d43ba8995c46d720$export$9c30223ca0a664fb;
+var $d43ba8995c46d720$export$463b44d9bf3628be;
+var $d43ba8995c46d720$export$34e0f9847d4c02dd;
+var $d43ba8995c46d720$export$46e4a324ac90507f;
+$d43ba8995c46d720$export$aa3e815946b80764 = `_7pkQ9q_card`;
+$d43ba8995c46d720$export$9c30223ca0a664fb = `_7pkQ9q_link`;
+$d43ba8995c46d720$export$463b44d9bf3628be = `_7pkQ9q_img`;
+$d43ba8995c46d720$export$34e0f9847d4c02dd = `_7pkQ9q_container`;
+$d43ba8995c46d720$export$46e4a324ac90507f = `_7pkQ9q_heading`;
+
+
+document.getElementById("app").innerHTML = `
+<div class="${(0, (/*@__PURE__*/$parcel$interopDefault($d43ba8995c46d720$exports))).card}">
+<img src="https://img.freepik.com/free-photo/tractor-agricultural-machine-cultivating-field_342744-896.jpg?w=996&t=st=1660627603~exp=1660628203~hmac=fd1cb771af3a1c50fab957c0b88646fbe15d27cd0dfe5753f96cab390f4e7857" alt="Trulli" class="${(0, (/*@__PURE__*/$parcel$interopDefault($d43ba8995c46d720$exports))).img}">
+
+  <div class="${(0, (/*@__PURE__*/$parcel$interopDefault($d43ba8995c46d720$exports))).container}">
+    <h1 class="${(0, (/*@__PURE__*/$parcel$interopDefault($d43ba8995c46d720$exports))).heading}">Our Products</h1> 
+    <p> To provide the best-tasting produce, we take extra care to choose and develop certain crop kinds where the excellent flavor is the focus.</p> 
+    <a 
+    class="${(0, (/*@__PURE__*/$parcel$interopDefault($d43ba8995c46d720$exports))).link}" href="https://parceljs.org" target="_blank" rel="noopener noreferrer">Learn More</a>
+  </div>
+</div>
+
+`;
+
+
+//# sourceMappingURL=index.js.map
